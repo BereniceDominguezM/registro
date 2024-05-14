706411f6fbf1d9f83cb907f9adc4d6a1a839da5a
BereniceDominguezM <167391428+BereniceDominguezM@users.noreply.github.com>
2024-05-14 01:47:14 -0600

    Add files via upload

---
+++ registro-de-productos/encabezado.php
@@ -0,0 +1,3 @@
+<h2 id="centrado">CONTROL DE PRODUCTOS</h2>
+<img src="img/compra.jpg" width="600" height="100" /> <!-- Cambio de atributo -->
+<!-- <h4 id="centrado">Registro del nuevo producto</h4> -->
